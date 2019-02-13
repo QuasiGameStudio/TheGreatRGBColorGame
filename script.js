8d41f929ae09ad5a66c2ecaec71ec298e4f12ff0
januar elsan <januarelsan@gmail.com>
2019-02-13 12:34:21 +0700

    Add Generate Random Color

--- script.js
+++ script.js
@@ -10,10 +10,13 @@ var colors = [
 ]
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = pickColor();
+var pickedColor;
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 
+
+colors = generateRandomColors(colors.length);
+pickedColor = pickColor();
 colorDisplay.textContent = pickedColor;
 
 for (var i = 0; i < squares.length; i++) {
@@ -47,5 +50,28 @@ function changeColors(color) {
 function pickColor() {
     var random = Math.floor(Math.random() * colors.length);
     return colors[random];
+}
+
+function generateRandomColors(num){
+    //make an array
+    var arr = [];
+    //repeat num times
+    for (var i = 0; i < num; i++) {
+        //get random color and push into array        
+        arr.push(randomColor());
+    }
+    //return that array
+    return arr;
+}
+
+function randomColor(){
+    //pic a "red" from 0-255;
+    var r = Math.floor(Math.random() * 256);
+    //pic a "green" from 0-255;
+    var g = Math.floor(Math.random() * 256);
+    //pic a "blue" from 0-255;
+    var b = Math.floor(Math.random() * 256);
+    
+    return "rgb(" + r + ", " + g + ", " + b + ")";
 
 }
